2d65de3d33f20c2d76126ce8a46d566985c5ae57
TroyChristian <apostasy8@gmail.com>
2019-08-09 19:40:06 -0400

    Make progress declaring intent, companion object.

--- app/src/main/java/com/example/myapplication/ListActivity.kt
+++ app/src/main/java/com/example/myapplication/ListActivity.kt
@@ -1,12 +1,37 @@
 package com.example.myapplication
 //comment for initial commit
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.myapplication.Movie.Movie
+import kotlinx.android.synthetic.main.activity_list.*
 
 class ListActivity : AppCompatActivity() {
+    var movieList = mutableListOf<Movie>()
+    var counter = 0
 
+    companion object{
+        const val REQUEST_CODE_EDIT_MOVIE = 2
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+        btn_add_movie.setOnClickListener{
+
+        }
+    }
+    fun createTextView(movie: Movie, index:Int){
+        var newMovieView = TextView(this)
+        newMovieView.textSize = 24f
+        newMovieView.id = index
+        newMovieView.text = movie.title
+
+        newMovieView.setOnClickListener{
+            var movieIntent = Intent(createPackageContext(this), ListActivity)
+            movieIntent.putExtra("movieKey", movieList[newMovieView.id])
+        }
+
     }
 }
