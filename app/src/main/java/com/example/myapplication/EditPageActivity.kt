20fa1257c7866ba69d296bc8b0a3c5c3767d1f8f
TroyChristian <apostasy8@gmail.com>
2019-08-10 01:48:42 -0400

    initialize EditActivity intent

--- app/src/main/java/com/example/myapplication/EditPageActivity.kt
+++ app/src/main/java/com/example/myapplication/EditPageActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
-
+import kotlinx.android.synthetic.main.activity_edit_page.et_movie_title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.Movie.Movie
 
 class EditPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_page)
+
+
+
+        var bundle: Bundle? = intent.extras
+        if(bundle != null){
+
+        }
+        fun loadMovie(movie:Movie){
+            et_movie_title.setText(movie.title)
+        }
     }
 }
+
