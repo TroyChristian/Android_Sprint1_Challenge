76c0cec51858267d1255664e612b34bd96058570
TroyChristian <apostasy8@gmail.com>
2019-08-10 02:08:34 -0400

    Resolve Scope Errors

--- app/src/main/java/com/example/myapplication/EditPageActivity.kt
+++ app/src/main/java/com/example/myapplication/EditPageActivity.kt
@@ -1,24 +1,34 @@
 package com.example.myapplication
-import kotlinx.android.synthetic.main.activity_edit_page.et_movie_title
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.Movie.Movie
+import kotlinx.android.synthetic.main.activity_edit_page.*
 
 class EditPageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_page)
-
-
-
-        var bundle: Bundle? = intent.extras
-        if(bundle != null){
+        /*WATCHED*/      fun createMovie(): Movie{
+            var newMovie = Movie(et_movie_title.text.toString(),false)
+            return newMovie
+        }
+        btn_save_movie.setOnClickListener{
+            var intentSaveMovie = Intent()
+            intentSaveMovie.putExtra("movie", createMovie())
 
         }
+
         fun loadMovie(movie:Movie){
             et_movie_title.setText(movie.title)
         }
+
+        var bundle: Bundle? = intent.extras
+        if(bundle != null){
+            loadMovie(bundle!!.getSerializable("tvMovie")as Movie)
+        }
+
     }
 }
 
